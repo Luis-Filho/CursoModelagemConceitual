99040bc758aee8618306244d90339bf83fc573b7
Luis <luis.filho@outlook.com.br>
2021-03-17 01:13:09 -0300

    Pedido, EstadoPagamento e Pagamento.

--- cursomc/src/main/java/com/projeto/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/projeto/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.projeto.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.projeto.cursomc.domain.Cidade;
 import com.projeto.cursomc.domain.Cliente;
 import com.projeto.cursomc.domain.Endereco;
 import com.projeto.cursomc.domain.Estado;
+import com.projeto.cursomc.domain.Pagamento;
+import com.projeto.cursomc.domain.PagamentoComBoleto;
+import com.projeto.cursomc.domain.PagamentoComCartao;
+import com.projeto.cursomc.domain.Pedido;
 import com.projeto.cursomc.domain.Produto;
+import com.projeto.cursomc.domain.enums.EstadoPagamento;
 import com.projeto.cursomc.domain.enums.TipoCliente;
 import com.projeto.cursomc.repositories.CategoriaRepository;
 import com.projeto.cursomc.repositories.CidadeRepository;
 import com.projeto.cursomc.repositories.ClienteRepository;
 import com.projeto.cursomc.repositories.EnderecoRepository;
 import com.projeto.cursomc.repositories.EstadoRepository;
+import com.projeto.cursomc.repositories.PagamentoRepository;
+import com.projeto.cursomc.repositories.PedidoRepository;
 import com.projeto.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 		
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -80,9 +92,25 @@ public class CursomcApplication implements CommandLineRunner{
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
 		clienteRepository.saveAll(Arrays.asList(cli1));
+		
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1); 
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 }
 
